4d506823514f7faa2366b2eb8e77e3cf64cdf55d
akilicaslan <88759885+akilicaslan@users.noreply.github.com>
2021-09-23 20:48:57 +0200

    update2

--- Scripts/aufgabe10.sql
+++ Scripts/aufgabe10.sql
@@ -31,9 +31,36 @@ fsi.datumzeit_ist_an_von AS datumzeit_ist_an,
 fsi.datumzeit_soll_an_von AS datumzeit_soll_an,
 TIMESTAMPDIFF(SECOND, fsi.datumzeit_soll_an_von,fsi.datumzeit_ist_an_von) AS delay
 
+FROM fahrzeiten_soll_ist fsi
+
+INNER JOIN haltepunkt hp ON hp.halt_punkt_id = fsi.halt_punkt_id_von
+INNER JOIN linie l ON l.fahrweg_id = fsi.fahrweg_id
+INNER JOIN haltestelle hs ON hs.halt_id = fsi.halt_id_von
+
+WHERE fsi.linie =2;
 
 
 
+
+
+ORDER BY delay DESC;
+
+CREATE TABLE to_map (
+
+lat double,
+lng double,
+name varchar(100),
+color varchar(100),
+note int);
+
+
+INSERT INTO to_map ( lat,lng,name,note)
+SELECT 
+a.gps_latitude,
+a.gps_longitude,
+a.halt_lang,
+a.delay FROM (SELECT * FROM abfrage) a;
+SELECT * FROM to_map ORDER BY note DESC;
 
 
 
